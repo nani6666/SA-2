4d088b3c143cfc8bff9c298acd872653b5d001d8
nani6666 <raviteja.m6666@gmail.com>
2018-05-17 02:17:40 +0530

    side panel change & accordion logic

--- src/app/pages/project/seller/seller.component.ts
+++ src/app/pages/project/seller/seller.component.ts
@@ -6,25 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller.component.scss']
 })
 export class SellerComponent implements OnInit {
-  public image:any;
+  public image: any;
+  companyInfoDiv: boolean;
+  companylogoDiv: boolean;
   
-  fileChange(input){
+  fileChange(input) {
       const reader = new FileReader();
       if (input.files.length) {
           const file = input.files[0];
           reader.onload = () => {
               this.image = reader.result;
           }
-          reader.readAsDataURL(file);           
+          reader.readAsDataURL(file);
       }
   }
 
-  removeImage():void{
+  removeImage(): void {
       this.image = '';
   }
   constructor() { }
 
   ngOnInit() {
+    this.companyInfoDiv = true ;
+  }
+ 
+  hideShow(id) {
+    const hideCompanyinfo = document.getElementById(id) ;
+    console.log(hideCompanyinfo.classList.length);
+    if (hideCompanyinfo.classList.length == 3) {
+    this.companyInfoDiv = true ;
+    this.companylogoDiv = true ;
+     (<any>hideCompanyinfo).classList.remove('companyInfo');
+    } else {
+        this.companyInfoDiv = false ;
+        this.companylogoDiv = false ;
+      (<any>hideCompanyinfo).classList.add('companyInfo');
+    }
   }
 
 }
